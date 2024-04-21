c27202fd53b1030704b04aa6fcd8f2d0290a3b39
Mingyuan Li <mli169@jhmi.edu>
2024-04-21 18:30:03 -0400

    Reduce number of repetitions

--- combine.R
+++ combine.R
@@ -8,13 +8,13 @@ load_as_list <- function(fname){
 }
 
 for(prefix in c("flex_CIs","S_CIs","para_CIs","sflex_CIs","sS_CIs")){
-filenames <- 1:100 %>% sprintf(paste(prefix,"_%03d.rda",sep=''), .)
+filenames <- 1:25 %>% sprintf(paste(prefix,"_%03d.rda",sep=''), .)
 comb_list <- lapply(filenames, load_as_list)
 varnames <- names(comb_list[[1]])
 comb_env <- new.env()
 for(var in varnames){
   assign(var, 
-  reduce(lapply(1:100, function(x) get(var,comb_list[[x]])), abind, along=1),
+  reduce(lapply(1:25, function(x) get(var,comb_list[[x]])), abind, along=1),
   comb_env
   )
 }
