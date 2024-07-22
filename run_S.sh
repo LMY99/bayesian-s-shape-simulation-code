14dd50ac93b5277d2e435ecc827aa80f2b4596b4
LMY99 <alfie_my@hotmail.com>
2024-07-22 17:45:15 -0400

    Specify R 4.3

--- run_S.sh
+++ run_S.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-module load conda_R
+module load conda_R/4.3
 Rscript "main_S.R" $seed
--- run_para.sh
+++ run_para.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-module load conda_R
+module load conda_R/4.3
 Rscript "main_para.R" $seed
