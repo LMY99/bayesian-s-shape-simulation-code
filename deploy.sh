4869f1f27a6f9e90f881fd46e38507908b1cf156
LMY99 <alfie_my@hotmail.com>
2024-08-01 11:37:28 -0400

    Change sim size from 100 to 30

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
 module load R/4.3
-for seed in {001..100}; do
+for seed in {001..030}; do
   if ! test -f "flex_CIs_${seed}.rda"; then
     sbatch --export="seed=${seed}" -J "flexA${seed}" --time=7-00:00:00 run_flex.sh
   fi
 done
-for seed in {001..100}; do
+for seed in {001..030}; do
   if ! test -f "sflex_CIs_${seed}.rda"; then
     sbatch --export="seed=${seed}" -J "sflexA${seed}" --time=7-00:00:00 run_flex_short.sh
   fi
 done
-for seed in {001..100}; do
+for seed in {001..030}; do
   if ! test -f "S_CIs_${seed}.rda"; then
     sbatch --export="seed=${seed}" -J "SA${seed}" --time=7-00:00:00 run_S.sh
   fi
 done
-for seed in {001..100}; do
+for seed in {001..030}; do
   if ! test -f "sS_CIs_${seed}.rda"; then
     sbatch --export="seed=${seed}" -J "sSA${seed}" --time=7-00:00:00 run_S_short.sh
   fi
 done
-for seed in {001..100}; do
+for seed in {001..030}; do
   if ! test -f "para_CIs_${seed}.rda"; then
     sbatch --export="seed=${seed}" -J "paraA${seed}" --time=7-00:00:00 run_para.sh
   fi
