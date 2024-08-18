952a8d497121ca11815695938ee158893e1bb6c7
LMY99 <alfie_my@hotmail.com>
2024-08-18 17:29:30 -0400

    Change to run FLEX only

--- deploy.sh
+++ deploy.sh
@@ -11,18 +11,18 @@ for seed in {001..100}; do
     sbatch --export="seed=${seed}" -J "sflexA${seed}" --time=7-00:00:00 run_flex_short.sh
   fi
 done
-for seed in {001..100}; do
-  if ! test -f "S_CIs_${seed}.rda"; then
-    sbatch --export="seed=${seed}" -J "SA${seed}" --time=7-00:00:00 run_S.sh
-  fi
-done
-for seed in {001..100}; do
-  if ! test -f "sS_CIs_${seed}.rda"; then
-    sbatch --export="seed=${seed}" -J "sSA${seed}" --time=7-00:00:00 run_S_short.sh
-  fi
-done
-for seed in {001..100}; do
-  if ! test -f "para_CIs_${seed}.rda"; then
-    sbatch --export="seed=${seed}" -J "paraA${seed}" --time=7-00:00:00 run_para.sh
-  fi
-done
+# for seed in {001..100}; do
+#   if ! test -f "S_CIs_${seed}.rda"; then
+#     sbatch --export="seed=${seed}" -J "SA${seed}" --time=7-00:00:00 run_S.sh
+#   fi
+# done
+# for seed in {001..100}; do
+#   if ! test -f "sS_CIs_${seed}.rda"; then
+#     sbatch --export="seed=${seed}" -J "sSA${seed}" --time=7-00:00:00 run_S_short.sh
+#   fi
+# done
+# for seed in {001..100}; do
+#   if ! test -f "para_CIs_${seed}.rda"; then
+#     sbatch --export="seed=${seed}" -J "paraA${seed}" --time=7-00:00:00 run_para.sh
+#   fi
+# done
